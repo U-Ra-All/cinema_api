05ee6bbea2a8e6476b3d6637a140d346693db712
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-14 08:46:49 +0300

    Implement GenreViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from cinema.models import Genre
+from cinema.serializers import GenreSerializer
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
