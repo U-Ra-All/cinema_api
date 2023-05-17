133f2fa9256b045f4e1074117fca85c7192bc4f6
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-17 17:40:17 +0300

    Dockerize app

--- cinema_api/settings.py
+++ cinema_api/settings.py
@@ -80,6 +80,13 @@ WSGI_APPLICATION = "cinema_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -129,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/vol/web/media"
 MEDIA_URL = "/media/"
 
 # Default primary key field type
