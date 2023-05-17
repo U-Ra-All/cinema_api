83d63a7c67e4d4eaa0b61c28adbc6e5ccf8c61c4
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-17 18:23:54 +0300

    Finish the app

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# cinema_api
+# Cinema API
 
+API service for cinema management written on DRF
+
+## Installation
+
+Python3 must be already installed
+
+```shell
+git clone https://github.com/U-Ra-All/cinema_api.git
+cd cinema_api
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 manage.py migrate
+python3 manage.py runserver # starts Django Server
+```
+
+Create a file called .env in the same folder as the settings file. 
+Make sure to have the following development-specific values in there.
+You can find the example in [.env_sample](.env_sample)
+
+```shell
+SECRET_KEY = "Your_Super_Secret_Key"
+```
+
+You can use the following superuser (or create another one by yourself):
+
+```shell
+Login: admin_user@mail.com
+Password: 7QancRe2
+```
+
+## Get Cinema API project from Docker Hub
+
+```shell
+docker pull uraall/docker-cinema-api
+```
+
+## Run with Docker
+
+```shell
+docker-compose build
+docker-compose up
+```
+
+## Getting access
+
+- create user via /api/user/register/
+- get access token via /api/user/token/
+
+## Features
+
+* JWT authenticated
+* Admin panel /admin/
+* Documentation is located at /api/doc/swagger/
+* Managing orders and tickets
+* Creating movies with genres, actors
+* Creating cinema halls
+* Adding movie sessions
+* Filtering movies and movie sessions
--- cinema_api/settings.py
+++ cinema_api/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a=#hu82#hj*dj(gtq5z!$gq4c&24bj3!xg59d=va$ykz+qg^2w"
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,16 +102,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME":
+            "django.contrib"
+            ".auth.password_validation"
+            ".UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME":
+            "django.contrib"
+            ".auth.password_validation"
+            ".MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME":
+            "django.contrib"
+            ".auth.password_validation"
+            ".CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME":
+            "django.contrib"
+            ".auth.password_validation"
+            ".NumericPasswordValidator",
     },
 ]
 
